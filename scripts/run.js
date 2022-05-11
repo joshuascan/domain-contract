7b18075c0b1fac37524dbcb5086d7a2efa46f157
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-11 14:11:17 -0700

    updates run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,20 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy("chrundle");
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
 
-  const txn = await domainContract.register("doom");
+  let txn = await domainContract.register("jscan", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await txn.wait();
 
-  const domainOwner = await domainContract.getAddress("doom");
-  console.log("Owner of domain:", domainOwner);
+  const address = await domainContract.getAddress("jscan");
+  console.log("Owner of domain:", address);
 
-  txn = await domainContract
-    .connect(randomPerson)
-    .setRecord("doom", "my domain now");
-  await txn.wait();
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
 
 const runMain = async () => {
