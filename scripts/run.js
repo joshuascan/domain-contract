47bab8d7476210cac4d77037410d7c23b37cb2e2
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-16 14:02:57 -0700

    updates run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,48 @@
 const main = async () => {
+  const [owner, superCoder] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy("chrundle");
   await domainContract.deployed();
 
-  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract owner:", owner.address);
 
   let txn = await domainContract.register("jscan", {
-    value: hre.ethers.utils.parseEther("0.01"),
+    value: hre.ethers.utils.parseEther("1234"),
   });
   await txn.wait();
 
-  const address = await domainContract.getAddress("jscan");
-  console.log("Owner of domain:", address);
-
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  try {
+    txn = await domainContract.connect(superCoder).withdraw();
+    await txn.wait();
+  } catch (error) {
+    console.log("Could not rob contract");
+  }
+
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+  console.log(
+    "Balance of owner before withdrawal:",
+    hre.ethers.utils.formatEther(ownerBalance)
+  );
+
+  txn = await domainContract.connect(owner).withdraw();
+  await txn.wait();
+
+  const contractBalance = await hre.ethers.provider.getBalance(
+    domainContract.address
+  );
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log(
+    "Contract balance after withdrawal:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+  console.log(
+    "Balance of owner after withdrawal:",
+    hre.ethers.utils.formatEther(ownerBalance)
+  );
 };
 
 const runMain = async () => {
